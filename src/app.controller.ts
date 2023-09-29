7d0188730ca1940305c2f56ea302cf0e1fccd92c
renatomattos2912@gwcloud.company <renatomattos.gwb.tfs@outlook.com>
2023-09-29 00:41:16 +0000

    feat: add controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/koritar')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
   }
